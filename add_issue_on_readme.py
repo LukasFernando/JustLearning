42f206fe635ffa6801562a7691e955a6778546f4
LukasFernando <94874696+LukasFernando@users.noreply.github.com>
2023-07-15 13:23:33 -0300

    Update add_issue_on_readme.py

--- add_issue_on_readme.py
+++ add_issue_on_readme.py
@@ -7,23 +7,3 @@ args = sys.argv
 
 print(args)
 
-
-
-
-# def Adding_issues_on_readme(file):
-#     new_file = ''
-#     for line in file.readlines():
-#
-#         new_file += line
-#
-#     return new_file
-#
-#
-# with open(file='README.md', mode='r', encoding='UTF-8') as file:
-#     new_file = Adding_issues_on_readme(file)
-#
-#
-# with open(file='README.md', mode='w', encoding='UTF-8') as file:
-#     file.write(new_file)
-
-
